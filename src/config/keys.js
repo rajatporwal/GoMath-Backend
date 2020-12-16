fec3eb6821b43d70034d0990bfc66f4f3d46f5dd
rajatporwal <rajatporwal1483@gmail.com>
2020-12-16 17:34:24 +0530

    feat(user): Added user routes and jwt auth

---
+++ src/config/keys.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://rajatporwal:Rajat1483@cluster0.edolx.mongodb.net/gomath?retryWrites=true&w=majority",
+  secretOrKey: "secret"
+};
--- src/index.js
+++ src/index.js
@@ -1,8 +1,33 @@
 const express = require("express");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const passport = require("passport");
+
+const users = require("./routes/api/users");
 
 const app = express();
 
-app.get("/", (req, res) => res.send("Hello from server"));
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+//DB Config
+const db = require("./config/keys").mongoURI;
+
+//Connect to MongoDB
+mongoose
+  .connect(db, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log("MongoDB Connected"))
+  .catch((err) => console.log(err));
+
+// Passport Middleware
+app.use(passport.initialize());
+
+// Passport Config
+require("./config/passport")(passport);
+
+// Use Routes
+app.use("/api/users", users);
 
 const port = process.env.PORT || 5000;
 
